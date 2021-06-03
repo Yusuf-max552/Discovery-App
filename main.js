ea82081dde34dfeb59f0fe9811810dc62c9d8516
Yusuf-max552 <82448349+Yusuf-max552@users.noreply.github.com>
2021-06-03 11:48:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function toggle(id) {
+    a = document.getElementById('toggle_'+id);
+    b = document.getElementById('display_'+id);
+      if (a.style.display=='block') {
+        a.style.display='none';
+        b.innerHTML='Show information';
+      }
+      else {
+        a.style.display='block';
+        b.innerHTML='Hide information';
+    }
+  }
+  function toggle(id) {
+    a = document.getElementById('toggle_'+id);
+    b = document.getElementById('display_'+id);
+      if (a.style.display=='block') {
+        a.style.display='none';
+        b.innerHTML='Show information';
+      }
+      else {
+        a.style.display='block';
+        b.innerHTML='Hide information';
+    }
+  }
